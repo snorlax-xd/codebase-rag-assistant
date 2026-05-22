5227959c621bea17565a356a03cf1d4b56999402
Rajul Tyagi <2024umt0180@iitjammu.ac.in>
2026-05-23 02:03:30 +0530

    add vercel domain to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,16 +3,21 @@ from app.api.repo_api import router as repo_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
 app.include_router(repo_router)
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://codebase-rag-assistant.vercel.app",
+        "https://*.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def root():
